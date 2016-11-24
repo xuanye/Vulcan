13c652b079787db4732379a6764a29ec62c77cc5
假正经哥哥 <xuanye.wan@gmail.com>
2016-11-24 17:18:54 +0800

    修改错误

--- sample/UUAC.DataAccess.Mysql/Repository/UserManageRepository.cs
+++ sample/UUAC.DataAccess.Mysql/Repository/UserManageRepository.cs
@@ -15,7 +15,7 @@ namespace UUAC.DataAccess.Mysql.Repository
         public async Task<PagedList<IUserInfo>> QueryUserList(string orgCode, string qText, PageView view)
         {
             string cols = @"user_uid as UserUid, full_name as FullName, account_type as AccountType,org_code as OrgCode,org_name as OrgName
-,is_admin as IsAdmin,sequence as Sequence,`status` as `Status`,last_modify_time as LastModifyTime,last_modify_user_id as LastModifyUserId
+,view_root_code as ViewRootCode,view_root_name as ViewRootName,sequence as Sequence,`status` as `Status`,last_modify_time as LastModifyTime,last_modify_user_id as LastModifyUserId
 ,last_modify_user_name as LastModifyUserName,user_num as UserNum , gender as Gender";
             string table = "user_info";
             string condition = "";
@@ -49,7 +49,7 @@ namespace UUAC.DataAccess.Mysql.Repository
         public async Task<IUserInfo> GetUserInfo(string userId)
         {
             string sql = @"SELECT user_uid as UserUid, full_name as FullName, account_type as AccountType,org_code as OrgCode,org_name as OrgName
-,is_admin as IsAdmin,sequence as Sequence,`status` as `Status`,last_modify_time as LastModifyTime,last_modify_user_id as LastModifyUserId
+,view_root_code as ViewRootCode,view_root_name as ViewRootName,sequence as Sequence,`status` as `Status`,last_modify_time as LastModifyTime,last_modify_user_id as LastModifyUserId
 ,last_modify_user_name as LastModifyUserName,user_num as UserNum , gender as Gender FROM user_info WHERE user_uid = @UserUId";
 
             var user = await base.GetAsync<UserInfo>(sql, new { UserUId = userId });
