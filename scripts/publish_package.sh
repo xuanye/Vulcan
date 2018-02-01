420e1f08966bb1198613e272501dcdc9d5db2af2
假正经哥哥 <xuanye.wan@gmail.com>
2018-02-01 14:58:35 +0800

    发布2.0.1版本

--- scripts/publish_package.sh
+++ scripts/publish_package.sh
@@ -15,13 +15,12 @@ dotnet restore ./Vulcan.DataAccess.sln
 
 dotnet build ./Vulcan.DataAccess/Vulcan.DataAccess.csproj -c Release
 
-revision="${TRAVIS_BUILD_NUMBER:=1}"
-versionNumber="1.0.${revision}"
+versionNumber="2.0.1"
 
 dotnet pack ./Vulcan.DataAccess/Vulcan.DataAccess.csproj -c Release -o ../$artifactsFolder --version-suffix=$versionNumber
 
 pwd
 
-if [ "$TRAVIS_BRANCH" == "master" ]; then
-    dotnet nuget push ./$artifactsFolder/Vulcan.DataAccess.${versionNumber}.nupkg -k $NUGET_KEY -s https://www.nuget.org
-fi
+
+dotnet nuget push ./$artifactsFolder/Vulcan.DataAccess.${versionNumber}.nupkg -k $NUGET_KEY -s https://www.nuget.org
+
