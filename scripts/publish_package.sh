69e5acab847f9c0bfd0dc45a96d070cbad7670f9
假正经哥哥 <xuanye.wan@gmail.com>
2018-06-06 14:10:41 +0800

    ⬆️ 升级dapper版本，修复insert时没有返回默认值时的异常处理

--- scripts/publish_package.sh
+++ scripts/publish_package.sh
@@ -15,7 +15,7 @@ dotnet restore ./Vulcan.DataAccess.sln
 
 dotnet build ./Vulcan.DataAccess/Vulcan.DataAccess.csproj -c Release
 
-versionNumber="2.0.1"
+versionNumber="2.1.0"
 
 dotnet pack ./Vulcan.DataAccess/Vulcan.DataAccess.csproj -c Release -o ../$artifactsFolder --version-suffix=$versionNumber
 
